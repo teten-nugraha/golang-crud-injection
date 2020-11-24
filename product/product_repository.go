933bbd3a54b91b0e27ce852b2e3713504d3eff74
Teten Nugraha <teten.nugraha@allianz.co.id>
2020-11-24 08:47:27 +0700

    repository

---
+++ product/product_repository.go
@@ -0,0 +1,35 @@
+package product
+
+import "github.com/jinzhu/gorm"
+
+type ProductRepository struct {
+	DB *gorm.DB
+}
+
+func ProviderProductRepository(DB *gorm.DB) ProductRepository {
+	return ProductRepository{DB: DB}
+}
+
+func (p *ProductRepository) FindAll() [] Product {
+	var products []Product
+	p.DB.Find(&products)
+
+	return products
+}
+
+func(p *ProductRepository) FindByID(id uint) Product {
+	var product Product
+	p.DB.First(&product, id)
+
+	return product
+}
+
+func (p *ProductRepository) Save(product Product) Product {
+	p.DB.Save(&product)
+
+	return product
+}
+
+func (p *ProductRepository) Delete(product Product) {
+	p.DB.Delete(&product)
+}
